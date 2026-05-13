05dea7da54dd643aa2de408038ef2192a62c5cc2
Zeynep Irem CAN <zeynepiremcan0238@gmail.com>
2026-05-13 23:16:21 +0300

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,12 @@
+/*
+====================================================
+INIT DATABASE
+====================================================
+
+Purpose:
+Creates the main database for Loan Analytics DWH project.
+
+====================================================
+*/
+
+CREATE DATABASE loan_analytics_dwh;
